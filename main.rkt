7fc843afaac08f1f2ff26d10335f2d24f3728c09
Metaxal <laurent.orseau@gmail.com>
2013-10-30 17:13:43 +0100

    include default-units.rkt

--- main.rkt
+++ main.rkt
@@ -1,6 +1,8 @@
 #lang racket/base
 
 (require "measure.rkt"
-         "convert.rkt")
+         "convert.rkt"
+         "default-units.rkt")
 (provide (all-from-out "measure.rkt"
-                       "convert.rkt"))
+                       "convert.rkt"
+                       "default-units.rkt"))
