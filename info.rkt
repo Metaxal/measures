f75e2361a767cab6fb662c761cc93d15b00c964a
Metaxal <laurent.orseau@gmail.com>
2018-01-20 19:27:28 +0000

    fix build deps

--- info.rkt
+++ info.rkt
@@ -4,7 +4,7 @@
   '("base"))
 
 (define build-deps
-  '("at-exp-lib" "rackunit-lib"))
+  '("at-exp-lib" "rackunit-lib" "scribble-lib" "racket-doc"))
 
 (define scribblings
   '(["README.scrbl" () (library) "measures"]))
